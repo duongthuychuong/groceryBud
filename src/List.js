f8e6d0aba0881e0561ffda29c15cf07eea1ccaca
chuongduong <duongthuychuong@gmail.com>
2022-11-25 08:02:04 +0700

    add items to list

--- src/List.js
+++ src/List.js
@@ -1,7 +1,32 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button
+                className="edit-btn"
+                type="button"
+                onClick={() => {
+                  console.log("Edit");
+                }}
+              >
+                <FaEdit></FaEdit>
+              </button>
+              <button className="delete-btn" type="button">
+                <FaEdit></FaEdit>
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
